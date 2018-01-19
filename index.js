05125b477fa814ce0672511c9021298169626f28
regbake <regbake@gmail.com>
2018-01-19 12:14:27 -0800

    installed karma and karma-mocha

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-var app = express(); //initiate express
+const app = express(); //initiate express
 
 //to use ejs for templating
 app.set("view engine", "ejs");
@@ -20,30 +20,30 @@ app.get("/", function(req, res){
   res.render("index", {
     city: " Seattle",
     names: names.data.names,
-    matchName: [1,2,3]
+    matchName: ["","",""]
   });
 });
 
 app.get("/api/names", function(req, res){
-  res.send({data: " Oakland??"});
+  res.send({data: names});
 });
 
 //send the name to the API using Post method (more secure than Get)
 app.post("/api/names", function(req, res){
-  let matchName = [req.body.data, 2, 3];
+  let matchName = [req.body.data, "", ""];
 
   //send the info to the front
-  res.render("index", {
-    city: " Seattle",
-    names: names.data.names,
-    matchName: matchName
-  });
-  //we are getting the name we want to search for
-  //find if it matches any names in the API then
-  //spit those matching names back to the front
+  // res.render("index", {
+  //   city: " Seattle",
+  //   names: names.data.names,
+  //   matchName: matchName
+  // });
 
-  // res.redirect("/");
+  res.redirect("/");
 });
 
 
 app.listen(3000);
+
+//for testing
+module.exports = app;
