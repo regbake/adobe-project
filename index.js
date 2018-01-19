f54a8e25c67214ad271e273b519056074c2bc55a
regbake <regbake@gmail.com>
2018-01-18 17:25:32 -0800

    added names.json file, body-parser, and fs

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require("express");
 var path = require("path");
-var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+
+var app = express(); //initiate express
 
 //to use ejs for templating
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+//for the default/home page
 app.get("/", function(req, res){
   res.render("index", {city: " Seattle"});
 });
+
+
 
 app.listen(3000);
