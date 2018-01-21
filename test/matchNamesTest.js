a20086f8a084959f9dc2e48beaf65317949cb9aa
regbake <regbake@gmail.com>
2018-01-21 12:25:01 -0800

    added karma-conf.js and karma-cli npm

--- test/matchNamesTest.js
+++ test/matchNamesTest.js
@@ -1,12 +1,14 @@
 const assert = require("assert");
 const matchNames = require("../matchNames").matchNames;
 
+//match names in the array of names
 matchNamesResult = matchNames("a", ["a", "b", "c"]);
 
-describe("A function", function(){
-  describe("Returns a matched names from array", function(){
-    it("should equal a", function(){
+describe("Test the name matching function", function(){
+  describe("Returns matched names from array", function(){
+    it("should equal 'a'", function(){
       assert.equal(matchNamesResult, "a");
+      //check type array
     });
   });
 });
